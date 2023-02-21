7f065a7932ddaed6decd4e7da58abfffbe50e1e8
cabraldasilvac <wcabraldasilvac@gmail.com>
2023-02-21 11:51:56 -0300

    Modifiquei o arquivo index.js renomeei o styles.css e modifiquei o MovieDetails.js colocando a Rota.

--- src/index.js
+++ src/index.js
@@ -4,12 +4,25 @@ import "./styles.css";
 import { Home } from "./views/Home";
 import reportWebVitals from "./reportWebVitals";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Header } from "./components/Header";
+import { MovieDetails } from "./views/MovieDetails";
+
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />,
+    element: <Header />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/movie/:id",
+        element: <MovieDetails />,
+      },
+    ],
   },
 ]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
--- src/views/MovieDetails.js
+++ src/views/MovieDetails.js
@@ -1,7 +1,8 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { MoviesService } from "../api/MoviesService";
-export const MovieDetail = () => {
+
+export const MovieDetails = () => {
   const [movie, setMovie] = useState({});
   const { id } = useParams();
   useEffect(() => {
